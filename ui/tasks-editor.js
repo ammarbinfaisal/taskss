06e22299337fd83dc75a53c5a8d649b876634b97
Malik Ammar Faisal <30975614+ammarbinfaisal@users.noreply.github.com>
2019-09-20 23:14:50 +0530

    fix escape and delete key working

--- ui/tasks-editor.js
+++ ui/tasks-editor.js
@@ -1,7 +1,7 @@
 const Configstore = require('configstore');
 
 const chalk = require('chalk');
-
+const log = require("../utils/log")
 const datastore = new Configstore('tasks');
 const screen = require('../utils/screen');
 
@@ -11,9 +11,9 @@ module.exports = (index, context) => {
 	const task = tasks[index];
 
 	/*
-        First array to containn keys of task object and
-        the second one to contain values
-    */
+        Outer array for y index and the inner for
+        starting and ending x index
+     */
 	const _task = [[], []];
 	if (!task) {
 		console.log(chalk.grey('\n\t:( no task with that index exists.\n'));
@@ -22,14 +22,18 @@ module.exports = (index, context) => {
 
 	screen.clear();
 
+	
 	/*
-        Outer array for y index and the inner for
-        starting and ending x index
-     */
+        First array to containn keys of task object and
+        the second one to contain values
+    */
 	const maxXValues = [null, null];
+
 	screen.cursorTo(0, 2);
+	
 	const maxKeyLen = Math.max.apply(null, Object.keys(task).map(key => key.length));
 	const minXValue = maxKeyLen + 4;
+	
 	Object.entries(task).forEach(([key, value]) => {
 		if (key !== 'done' && key !== 'index') {
 			maxXValues.push(minXValue + value.length);
@@ -43,18 +47,21 @@ module.exports = (index, context) => {
 			screen.write(`  ${chalk.cyan(`${key}`)} ${value}\n`);
 		}
 	});
+
+	// x refers to x index of cursor on the screen
+	// y refers to y index of cursor on the screen
 	const positionCursor = (x, y) => {
 		y = y || screen.y;
 		y = y < 2 ? 2 : y > 3 ? 3 : y;
-		x = x || screen.x; // If not passed
+		x = x || screen.x;
 		// prettier-ignore
-		x = x ? // If screen.x gives 0
-			x > maxXValues[y] ? // And if is greater than max allowed value at that line
-				maxXValues[y] : // Then set it as max allowed value
+		x = x ?
+			x > maxXValues[y] ? // If x is greater than max value at that line
+				maxXValues[y] : // Then set it as max value
 				x < minXValue ? // And if is less than min x value
 					minXValue : // Then set it as min x value
 					x : // Otherwise let it be x
-			minXValue; // If not passed then make it min x value
+			minXValue; // If screen.x is 0
 		screen.cursorTo(x, y);
 	};
 
@@ -78,6 +85,7 @@ module.exports = (index, context) => {
 		const _x = screen.x;
 		const _y = screen.y;
 		const i = _y - 2;
+		log(`x: ${_x} \t key: ${key.name} \t chunk: ${chunk}`)
 		if (key && key.name === 'up') {
 			positionCursor(undefined, _y - 1);
 		} else if (key && key.name === 'down') {
@@ -87,17 +95,18 @@ module.exports = (index, context) => {
 		} else if (key && key.name === 'right') {
 			positionCursor(_x + 1);
 		} else if (key && key.name === 'backspace') {
-			maxXValues[_y]--;
+			if(_task[1][i]) maxXValues[_y]--;
 			_task[1][i] = _task[1][i].substring(0, _x - minXValue - 1) + _task[1][i].substring(_x - minXValue);
 			positionCursor(minXValue);
 			screen.write(_task[1][i] + ' ');
 			positionCursor(_x - 1);
 		} else if (key && key.name === 'delete') {
-			maxXValues[_y]--;
+			if(_task[1][i]) maxXValues[_y]--;
 			_task[1][i] = _task[1][i].substring(0, _x - minXValue) + _task[1][i].substring(_x - minXValue + 1);
 			positionCursor(minXValue);
 			screen.write(_task[1][i] + ' ');
-			positionCursor(_x - 1);
+			log("writing \"" + _task[1][i] + '\"<space>')
+			positionCursor(_x);
 		} else if (key && key.name === 'escape' && context === 'list') {
 			screen.removeListener('keypress', keypressHandler);
 		} else if (chunk && chunk.length === 1 && !key.ctrl) {
