6a9c4330e610036c2ce6bd4d07dca9bcdc6955de
Malik Ammar Faisal <30975614+ammarbinfaisal@users.noreply.github.com>
2019-09-20 23:15:46 +0530

    npm run xo

--- ui/tasks-editor.js
+++ ui/tasks-editor.js
@@ -1,7 +1,8 @@
 const Configstore = require('configstore');
 
 const chalk = require('chalk');
-const log = require("../utils/log")
+const log = require('../utils/log');
+
 const datastore = new Configstore('tasks');
 const screen = require('../utils/screen');
 
@@ -22,7 +23,6 @@ module.exports = (index, context) => {
 
 	screen.clear();
 
-	
 	/*
         First array to containn keys of task object and
         the second one to contain values
@@ -30,10 +30,10 @@ module.exports = (index, context) => {
 	const maxXValues = [null, null];
 
 	screen.cursorTo(0, 2);
-	
+
 	const maxKeyLen = Math.max.apply(null, Object.keys(task).map(key => key.length));
 	const minXValue = maxKeyLen + 4;
-	
+
 	Object.entries(task).forEach(([key, value]) => {
 		if (key !== 'done' && key !== 'index') {
 			maxXValues.push(minXValue + value.length);
@@ -48,7 +48,7 @@ module.exports = (index, context) => {
 		}
 	});
 
-	// x refers to x index of cursor on the screen
+	// X refers to x index of cursor on the screen
 	// y refers to y index of cursor on the screen
 	const positionCursor = (x, y) => {
 		y = y || screen.y;
@@ -85,7 +85,7 @@ module.exports = (index, context) => {
 		const _x = screen.x;
 		const _y = screen.y;
 		const i = _y - 2;
-		log(`x: ${_x} \t key: ${key.name} \t chunk: ${chunk}`)
+		log(`x: ${_x} \t key: ${key.name} \t chunk: ${chunk}`);
 		if (key && key.name === 'up') {
 			positionCursor(undefined, _y - 1);
 		} else if (key && key.name === 'down') {
@@ -95,17 +95,17 @@ module.exports = (index, context) => {
 		} else if (key && key.name === 'right') {
 			positionCursor(_x + 1);
 		} else if (key && key.name === 'backspace') {
-			if(_task[1][i]) maxXValues[_y]--;
+			if (_task[1][i]) maxXValues[_y]--;
 			_task[1][i] = _task[1][i].substring(0, _x - minXValue - 1) + _task[1][i].substring(_x - minXValue);
 			positionCursor(minXValue);
 			screen.write(_task[1][i] + ' ');
 			positionCursor(_x - 1);
 		} else if (key && key.name === 'delete') {
-			if(_task[1][i]) maxXValues[_y]--;
+			if (_task[1][i]) maxXValues[_y]--;
 			_task[1][i] = _task[1][i].substring(0, _x - minXValue) + _task[1][i].substring(_x - minXValue + 1);
 			positionCursor(minXValue);
 			screen.write(_task[1][i] + ' ');
-			log("writing \"" + _task[1][i] + '\"<space>')
+			log('writing "' + _task[1][i] + '"<space>');
 			positionCursor(_x);
 		} else if (key && key.name === 'escape' && context === 'list') {
 			screen.removeListener('keypress', keypressHandler);
